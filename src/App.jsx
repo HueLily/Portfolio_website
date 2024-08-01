4006017d47afc9c7ca328e3a8acf6afaa116ec90
nguyenhue <huenguyen_2026@depauw.edu>
2024-08-01 16:02:04 -0400

    add components to app.js and setup app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
 import './App.css'
+import Banner from './components/Banner'
+import Contact from './components/Contact'
+import Experience from './components/Experience'
+import Leadership from './components/Leadership'
+import Navbar from './components/Navbar'
+import Project from './components/Project'
+import Qualification from './components/Qualification'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App-container">
-      
+      <Navbar />
+      <Banner />
+      <Qualification />
+      <Experience />
+      <Project />
+      <Leadership />
+      <Contact />
     </div>
   )
 }
+
 
 export default App
