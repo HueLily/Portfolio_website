e7d62c9f220cefade194e7e88f3e2827bd7cd538
nguyenhue <huenguyen_2026@depauw.edu>
2024-08-02 02:21:34 -0400

    create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
-import './App.css'
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Banner from './components/Banner'
-import Contact from './components/Contact'
-import Experience from './components/Experience'
-import Leadership from './components/Leadership'
-import NavigationBar from './components/Navbar'
-import Project from './components/Project'
-import Qualification from './components/Qualification'
+import NavigationBar from "./components/Navbar";
 
 function App() {
-
   return (
     <div className="App-container">
       <NavigationBar />
-      {/* <Banner />
-      <Qualification />
-      <Experience />
-      <Project />
-      <Leadership />
-      <Contact /> */}
     </div>
-  )
+  );
 }
 
-
-export default App
+export default App;
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Button, Nav, Navbar, Container } from "react-bootstrap";
+import { Nav, Navbar, Container } from "react-bootstrap";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 
@@ -26,7 +26,81 @@ const NavigationBar = () => {
   };
 
   return (
-    <></>
+    <Router>
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+        <Navbar.Brand href="#home">
+          <h2>HUE (LILY) THU NGUYEN</h2>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link
+              href="#home"
+              className={
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#qualification"
+              className={
+                activeLink === "qualification"
+                  ? "active navbar-link"
+                  : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("qualification")}
+            >
+              About
+            </Nav.Link>
+            <Nav.Link
+              href="#experience"
+              className={
+                activeLink === "experience"
+                  ? "active navbar-link"
+                  : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("experience")}
+            >
+              Experience
+            </Nav.Link>
+            <Nav.Link
+              href="#project"
+              className={
+                activeLink === "project" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("project")}
+            >
+              Projects
+            </Nav.Link>
+            <Nav.Link
+              href="#leadership"
+              className={
+                activeLink === "leadership"
+                  ? "active navbar-link"
+                  : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("leadership")}
+            >
+              Leadership
+            </Nav.Link>
+            <Nav.Link
+              href="#contact"
+              className={
+                activeLink === "contact" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("contact")}
+            >
+              Contact
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Router>
   );
 };
 export default NavigationBar;
