886447a7e52bbbd329dad63b77dc3d7b9d994c99
nguyenhue <huenguyen_2026@depauw.edu>
2024-08-08 17:02:01 -0400

    add images to qualification and fix the change in last and first child of skill box

--- src/components/Qualification/index.jsx
+++ src/components/Qualification/index.jsx
@@ -2,6 +2,24 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import circleImg from "../../assets/me-img.jpg";
 import Marquee from "react-fast-marquee";
+import java from '../../assets/java.svg';
+import python from '../../assets/python.svg';
+import cplusplus from '../../assets/c++.svg';
+import html from '../../assets/html5.svg';
+import css from "../../assets/css3.svg";
+import javascript from '../../assets/javascript.svg';
+import typescript from '../../assets/typescript.svg';
+import react from '../../assets/react.svg';
+import bootstrap from "../../assets/bootstrap.svg";
+import tailwind from "../../assets/tailwind.svg";
+import node from "../../assets/node.svg";
+import spring from "../../assets/spring.svg";
+import mongo from "../../assets/mongo.svg";
+import postgre from "../../assets/postgre.svg";
+import github from "../../assets/github.svg";
+import git from "../../assets/git.svg";
+import netlify from "../../assets/netlify.svg";
+import tableau from "../../assets/tableau.svg";
 import "./index.css";
 
 const Qualification = () => {
@@ -53,86 +71,112 @@ const Qualification = () => {
             >
               <Col xs={4} md={2} className="tech-icons">
                 <div className="item">
+                  <img src={java} alt=""></img>
                   <p>Java</p>
                 </div>
               </Col>
               <Col xs={4} md={2} className="tech-icons">
                 <div className="item">
+                <img src={python} alt=""></img>
                   <p>Python</p>
                 </div>
               </Col>
               <Col xs={4} md={2} className="tech-icons">
                 <div className="item">
+                <img src={cplusplus} alt=""></img>
                   <p>C/C++</p>
                 </div>
               </Col>
               <Col xs={4} md={2} className="tech-icons">
                 <div className="item">
+                <img src={html} alt=""></img>
                   <p>HTML5</p>
                 </div>
               </Col>
               <Col xs={4} md={2} className="tech-icons">
                 <div className="item">
+                <img src={css} alt=""></img>
                   <p>CSS3</p>
                 </div>
               </Col>
               <Col xs={4} md={2} className="tech-icons">
                 <div className="item">
+                <img src={javascript} alt=""></img>
                   <p>Javascript</p>
                 </div>
               </Col>
               <Col xs={4} md={2} className="tech-icons">
                 <div className="item">
-                  <p>Node JS</p>
+                <img src={typescript} alt=""></img>
+                  <p>TypeScript</p>
                 </div>
               </Col>
               <Col xs={4} md={2} className="tech-icons">
                 <div className="item">
+                <img src={react} alt=""></img>
                   <p>React JS</p>
                 </div>
               </Col>
               <Col xs={4} md={2} className="tech-icons">
                 <div className="item">
-                  <p>Angular</p>
-                </div>
-              </Col>
-              <Col xs={4} md={2} className="tech-icons">
-                <div className="item">
+                <img src={bootstrap} alt=""></img>
                   <p>Bootstrap</p>
                 </div>
               </Col>
               <Col xs={4} md={2} className="tech-icons">
                 <div className="item">
-                  <p>PostgreSQL</p>
+                <img src={tailwind} alt=""></img>
+                  <p>Tailwind</p>
                 </div>
               </Col>
               <Col xs={4} md={2} className="tech-icons">
                 <div className="item">
-                  <p>MongoDB</p>
-                </div>
-              </Col>
-              <Col xs={4} md={2} className="tech-icons">
-                <div className="item">
+                <img src={node} alt=""></img>
                   <p>Node JS</p>
                 </div>
               </Col>
               <Col xs={4} md={2} className="tech-icons">
                 <div className="item">
+                <img src={spring} alt=""></img>
                   <p>Spring Boot</p>
                 </div>
               </Col>
               <Col xs={4} md={2} className="tech-icons">
                 <div className="item">
+                <img src={mongo} alt=""></img>
+                  <p>MongoDB</p>
+                </div>
+              </Col>
+              <Col xs={4} md={2} className="tech-icons">
+                <div className="item">
+                <img src={postgre} alt=""></img>
+                  <p>PostgreSQL</p>
+                </div>
+              </Col>
+              <Col xs={4} md={2} className="tech-icons">
+                <div className="item">
+                <img src={github} alt=""></img>
+                  <p>GitHub</p>
+                </div>
+              </Col>
+              <Col xs={4} md={2} className="tech-icons">
+                <div className="item">
+                <img src={git} alt=""></img>
                   <p>Git</p>
                 </div>
               </Col>
-              
               <Col xs={4} md={2} className="tech-icons">
                 <div className="item">
-                  <p>Node JS</p>
+                <img src={netlify} alt=""></img>
+                  <p>Netlify</p>
                 </div>
               </Col>
-              
+              <Col xs={4} md={2} className="tech-icons">
+                <div className="item">
+                <img src={tableau} alt=""></img>
+                  <p>Tableau</p>
+                </div>
+              </Col>
             </Row>
           </Marquee>
         </div>
