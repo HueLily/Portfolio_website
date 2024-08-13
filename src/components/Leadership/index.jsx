c7b7d6f2f65eddaffe255f3d01b2c6b34c08e97a
nguyenhue <huenguyen_2026@depauw.edu>
2024-08-13 18:00:59 -0400

    create 2 row of hexagon component and add images to hexagon

--- src/components/Leadership/index.jsx
+++ src/components/Leadership/index.jsx
@@ -31,59 +31,6 @@ const Leadership = () => {
     <section className="leadership" id="leader">
       <Container>
         <h2>Leadership Experience</h2>
-
-        {/* <Carousel
-            className="carousel-container"
-              additionalTransfrom={0}
-              arrows
-              autoPlaySpeed={3000}
-              centerMode={false}
-              containerClass="container"
-              infinite
-              keyBoardControl
-              pauseOnHover
-              renderArrowsWhenDisabled={false}
-              renderButtonGroupOutside={false}
-              responsive={{
-                desktop: {
-                  breakpoint: {
-                    max: 3000,
-                    min: 1024,
-                  },
-                  items: 1,
-                },
-                mobile: {
-                  breakpoint: {
-                    max: 464,
-                    min: 0,
-                  },
-                  items: 1,
-                },
-                tablet: {
-                  breakpoint: {
-                    max: 1024,
-                    min: 464,
-                  },
-                  items: 1,
-                },
-              }}
-              showDots={false}
-              swipeable
-              height='60%'
-            >
-              <img
-                src={wicsEvent}
-              className="carousel-img"
-              />
-              <img
-                 src={wicsEvent}
-                className="carousel-img"
-              />
-              <img
-                 src={wicsEvent}
-                className="carousel-img"
-              />
-            </Carousel> */}
         <Row>
           <Carousel indicators={false} data-bs-theme="dark">
             <Carousel.Item interval={2500} style={carouselStyle}>
@@ -115,10 +62,19 @@ const Leadership = () => {
           </Carousel>
         </Row>
         <Row>
-          <div className="hexa gon"></div>
-          <div className="hexa gon"></div>
-          <div className="hexa gon"></div>
-          <div className="hexa gon"></div>
+          <div className="hexa gon">
+            <img className ="hexa-img" src={wecodeLogo} alt=""></img>
+          </div>
+          <div className="hexa gon"><img className ="hexa-img" src={wecodeLogo} alt=""></img></div>
+          <div className="hexa gon"><img className ="hexa-img" src={wecodeLogo} alt=""></img></div>
+          <div className="hexa gon"><img className ="hexa-img" src={wecodeLogo} alt=""></img></div>
+          <div className="hexa gon"><img className ="hexa-img" src={wecodeLogo} alt=""></img></div>
+          <div className="hexa gon"><img className ="hexa-img" src={wecodeLogo} alt=""></img></div>
+        </Row>
+        <Row className="second-row">
+          <div className="hexa gon"><img className ="hexa-img" src={wecodeLogo} alt=""></img></div>
+          <div className="hexa gon"><img className ="hexa-img" src={wecodeLogo} alt=""></img></div>
+          <div className="hexa gon"><img className ="hexa-img" src={wecodeLogo} alt=""></img></div>
         </Row>
       </Container>
     </section>
