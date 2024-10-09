160a70f568cf9b9f54085a45fdf39504d7b555c9
nguyenhue <huenguyen_2026@depauw.edu>
2024-10-09 07:39:23 -0400

    update project finbud AI

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import musicApp from "../../assets/music-app.png";
 import marketApp from "../../assets/market-app.png";
-import organicApp from "../../assets/organic.jpg";
+import finbud from "../../assets/finbud.png";
 import dpuHack from "../../assets/DePauw-hack.png";
 import safeRoute from "../../assets/safe-route.jpg";
 import googleAPI from "../../assets/google-api.jpg";
@@ -12,27 +12,19 @@ import { Parallax } from "../../util/Parallax";
 const Project = () => {
   const projects = [
     {
+      img: finbud,
+      title: "FinBud AI",
+      desc: "FinBud AI is a full-stack financial app using AI power, Vue.JS, React, MongoDB, Passprt and Netlifty to track stock on real-time, get personalized trade suggestions, and get financial guidance by AI Chatbot",
+      live:"https://finbud.pro/",
+      github: "https://github.com/finbud2024/Finbud"
+    },
+
+    {
       img: musicApp,
       title: "Melify App",
       desc: "Melify is a full-stack music app using Java (Spring Boot) and React, integrating Spotify and OpenAI APIs for personalized playlist recommendations based on emotions, with features for streaming, listening, and sharing tracks or playlists.",
       live:"https://melify-app.netlify.app/",
       github: "https://github.com/SWE1dePauwMusic/music_app"
-    },
-
-    {
-      img: marketApp,
-      title: "Renoon Same",
-      desc: "Renoon is a university-focused e-commerce website using React and Node, featuring buying, selling, and exchange functionalities with secure authentication and interactive post management.",
-      live:"https://github.com/Renoon-same/frontend",
-      github: "https://github.com/Renoon-same/frontend"
-    },
-
-    {
-      img: organicApp,
-      title: "FireGuard",
-      desc: "FireGuard is a full-stack app using React, Node, and Express, integrating Google Maps and Weather APIs to provide residents with real-time fire information, send alerts, and enable firefighters to track fire spread, predict temperature changes, and identify potential future fires.",
-      live:"https://github.com/HueLily/fireguard",
-      github: "https://github.com/HueLily/fireguard"
     },
 
     {
@@ -43,6 +35,13 @@ const Project = () => {
       github: "https://github.com/HueLily/Hackathon_website"
     },
 
+    {
+      img: marketApp,
+      title: "Renoon Same",
+      desc: "Renoon is a university-focused e-commerce website using React and Node, featuring buying, selling, and exchange functionalities with secure authentication and interactive post management.",
+      live:"https://github.com/Renoon-same/frontend",
+      github: "https://github.com/Renoon-same/frontend"
+    },
     {
       img: googleAPI,
       title: "Google API Workshop",
